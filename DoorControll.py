192f5c1003d98408139d9c12463e3ad1d149e3e0
Dorian <rolaulten@gmail.com>
2021-05-15 12:15:11 -0700

    Typo Fix

--- DoorControll.py
+++ DoorControll.py
@@ -1,8 +1,13 @@
+#Script to open a chicken door.
+#writen by Dorian Goettler, free to use unter the MIT licence.
+
+#Requires: l298n, time.
+
 
 from l298n import l298n
 import time
 motor1 = (20,21)
 
-motor1.start
+motor1.forwards
 time.sleep(10)
 motor1.stop
